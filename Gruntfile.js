519063b973b84e720fa4afffc358941d503b1c06
MattSchlobohm <matt@schlobohm.net>
2013-08-03 20:05:54 -0300

    tweak grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,8 +26,7 @@ module.exports = function(grunt) {
                     host: 'markupmadness.com'
                 },
                 src: 'build',
-                dest: '/html/.',
-                keep: ['/html/old']
+                dest: 'html'
             }
         }
     });
